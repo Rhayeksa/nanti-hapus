0507ea1191f036225f62bfdc7ffba439f03930a1
rhayeksa <rhayeksads@gmail.com>
2022-04-16 01:06:06 +0700

    setup model hero dan orang

--- hero/models.py
+++ hero/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hero(models.Model):
+    role_value = [
+        ('Kesatria', 'Kesatria'),
+        ('Pemanah', 'Pemanah'),
+        ('Penyihir', 'Penyihir')
+    ]
+
+    nama = models.CharField(max_length=50)
+    role = models.CharField(
+        max_length=15,
+        choices=role_value,
+        default=role_value[0]
+    )
+    deskripsi = models.TextField()
+
+    class Meta:
+        db_table = 'hero'
+
+    def __str__(self):
+        return "{}".format(self.id)
--- orang/models.py
+++ orang/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from hero.models import Hero
+
 # Create your models here.
+
+
+class Orang(models.Model):
+    username = models.CharField(primary_key=True, max_length=50)
+    email = models.EmailField(max_length=254)
+    nama = models.CharField(max_length=255)
+    penjelasan = models.TextField()
+    id_hero = models.ForeignKey(
+        Hero,
+        on_delete=models.prefetch_related_objects,
+        db_column='id_hero',
+    )
+
+    class Meta:
+        db_table = 'orang'
+
+    def __str__(self):
+        return "{}".format(self.id)
