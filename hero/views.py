b2f8f5d172f66e04168cead58f340d61ac33d549
rhayeksa <rhayeksads@gmail.com>
2022-04-16 01:18:50 +0700

    setup views hero dan orang

--- hero/views.py
+++ hero/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets, filters
+
+from hero.models import Hero
+from hero.serializers import Serializer
+
 
 # Create your views here.
+
+
+class ListCreateAPIViewHero(viewsets.ViewSetMixin, generics.ListCreateAPIView):
+    queryset = Hero.objects.all()
+    serializer_class = Serializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nama']
+    ordering_fields = ['id', 'nama', 'role']
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class RetrieveUpdateDestroyAPIViewHero(viewsets.ModelViewSet, generics.RetrieveUpdateDestroyAPIView):
+    queryset = Hero.objects.all()
+    serializer_class = Serializer
+    # permission_classes = [permissions.IsAuthenticated]
--- orang/views.py
+++ orang/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics, filters, permissions
+from orang.models import Orang
 
+from orang.serializers import Serializer
 # Create your views here.
+
+
+class ListCreateAPIViewOrang(viewsets.ViewSetMixin, generics.ListCreateAPIView):
+    queryset = Orang.objects.all()
+    serializer_class = Serializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nama']
+    ordering_fields = ['id', 'nama', 'id_hero']
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class RetrieveUpdateDestroyAPIViewOrang(viewsets.ModelViewSet, generics.RetrieveUpdateDestroyAPIView):
+    queryset = Orang.objects.all()
+    serializer_class = Serializer
+    # permission_class = [permissions.IsAuthenticated]
